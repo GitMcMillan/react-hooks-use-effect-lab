93fb6df1e12dec502a999d8a52416c5b0eab8b8e
alexandermcmillan <mcmillancontact@protonmail.com>
2024-02-21 13:21:03 -0800

    completed lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // Check if time remaining is greater than 0
+      if (timeRemaining > 0) {
+        // Decrement the time by 1
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        // Reset time remaining to 10 for the next question 
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+
+    // Cleanup function to clear the timeout 
+    return () => clearTimeout(timer)
+  }, [timeRemaining, onAnswered])
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
+
+
 
   const { id, prompt, answers, correctIndex } = question;
 
